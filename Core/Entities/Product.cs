dbe177a44a68722b4ca7a8cd2d65c89f7190a3ba
33185 <hameed_20180216@fci.helwan.edu.eg>
2023-03-27 20:22:24 +0200

    add Readme

--- Core/Entities/Product.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace E_Commerce_API.Entities
-{
-    public class Product
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
---
+++ README.md
@@ -0,0 +1,4 @@
+# E-Commerce-API
+
+E-Commerce with payment integration using stripe gateway using ASP.NET Core 6 API
+
